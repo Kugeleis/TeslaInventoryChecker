1f5d48c14dd50823238a0e7b31e4dfda7b10d4f1
TJ <tj@tnoor.co>
2021-08-13 09:45:09 -0400

    Added Support For Multiple Zips
    - added support for multiple zips from the same config file
    - added support for split results from Tesla API

--- check_inventory.py
+++ check_inventory.py
@@ -27,64 +27,82 @@ config = configparser.ConfigParser()
 config.read(config_file)
 model = config['Inventory']['model']
 region = config['Inventory']['region']
-zipcode = config['Inventory']['zip']
+zipcodes = config['Inventory']['zip'].split(",")
 zip_range = config['DEFAULT']['range']
 condition = config['DEFAULT']['condition']
 
-print(
-    f'Searching for {condition} Tesla {model.upper()} within {zip_range} miles of {zipcode}')
-
-search_query = {
-    "query": {
-        "model": model,
-        "condition": condition,
-        "options": {},
-        "arrangeby": "Price",
-        "order": "asc",
-        "market": config['DEFAULT']['market'],
-        "language": "en",
-        "super_region": config['DEFAULT']['super_region'],
-        "zip": zipcode,
-        "range": int(zip_range),
-        "region": region
-    },
-    "offset": 0,
-    "count": 50,
-    "outsideOffset": 0,
-    "outsideSearch": False
-}
-
-api_path = "/inventory/api/v1/inventory-results?query=" + \
-    urllib.parse.quote_plus(json.dumps(search_query))
-
-search_url = f'https://www.tesla.com/inventory/{condition}/{model}?arrangeby=phl&zip={zipcode}&range={zip_range}'
-conn = http.client.HTTPSConnection("www.tesla.com")
-payload = ''
-headers = {
-    'Referer': search_url,
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
-    'dnt': '1'
-}
-
 while True:
-    conn.request("GET", api_path, payload, headers)
-    res = conn.getresponse()
-    data = res.read()
-    search_results = json.loads(
-        data, object_hook=lambda d: SimpleNamespace(**d))
-
-    number_of_results = int(search_results.total_matches_found)
-
-    if number_of_results > 0:
+    for zipcode in zipcodes:
         print(
-            f"Inventory Found - {str(number_of_results)} {condition} {model} found at {datetime.now()}")
-        discord.send_message(config['Discord']['api'], json.loads(
-            (json.dumps(search_query)), object_hook=lambda d: SimpleNamespace(**d)), search_results)
-    else:
-        print(f"No {condition} {model} vehicles were found at {datetime.now()}")
+            f'Searching for {condition} Tesla {model.upper()} within {zip_range} miles of {zipcode}')
+
+        search_query = {
+            "query": {
+                "model": model,
+                "condition": condition,
+                "options": {},
+                "arrangeby": "Price",
+                "order": "asc",
+                "market": config['DEFAULT']['market'],
+                "language": "en",
+                "super_region": config['DEFAULT']['super_region'],
+                "zip": zipcode,
+                "range": int(zip_range),
+                "region": region
+            },
+            "offset": 0,
+            "count": 50,
+            "outsideOffset": 0,
+            "outsideSearch": False
+        }
+
+        api_path = "/inventory/api/v1/inventory-results?query=" + \
+            urllib.parse.quote_plus(json.dumps(search_query))
+
+        search_url = f'https://www.tesla.com/inventory/{condition}/{model}?arrangeby=phl&zip={zipcode}&range={zip_range}'
+        conn = http.client.HTTPSConnection("www.tesla.com")
+        payload = ''
+        headers = {
+            'Referer': search_url,
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36',
+            'dnt': '1'
+        }
+
+        conn.request("GET", api_path, payload, headers)
+        res = conn.getresponse()
+        data = res.read()
+        search_results = json.loads(
+            data, object_hook=lambda d: SimpleNamespace(**d))
+
+        total_matches = int(search_results.total_matches_found)
+        total_exact = 0
+        total_approximate = 0
+        total_approximateOutside = 0
+
+        if type(search_results.results) is not list:
+            total_exact = len(search_results.results.exact)
+            total_approximate = len(search_results.results.approximate)
+            total_approximateOutside = len(
+                search_results.results.approximateOutside)
+
+        total_split_matches = total_exact + total_approximate + total_approximateOutside
+
+        if total_matches > 0:
+            print(
+                f"Inventory Found - {str(total_matches)} {condition} {model} found at {datetime.now()}")
+            discord.send_message(config['Discord']['api'], json.loads(
+                (json.dumps(search_query)), object_hook=lambda d: SimpleNamespace(**d)), search_results)
+        elif total_split_matches > 0:
+            print(
+                f"Split Inventory Found - {str(total_split_matches)} {condition} {model} found at {datetime.now()}")
+            discord.send_message_split_results(config['Discord']['api'], json.loads(
+                (json.dumps(search_query)), object_hook=lambda d: SimpleNamespace(**d)), search_results)
+        else:
+            print(
+                f"No {condition} {model} vehicles were found at {datetime.now()}")
 
     if args.repeat == None:
         break
     else:
-        print(f"Waiting for {args.repeat} seconds ...")
+        print(f">> Waiting for {args.repeat} seconds ...")
         time.sleep(int(args.repeat))
--- discord.py
+++ discord.py
@@ -1,13 +1,37 @@
 import json
 import http.client
 
+separator = ", "
+
+
+def build_vehicle_card(v, zip_code):
+    desc = f"→ VIN: {v.VIN}\n"
+    desc += f"→ Odometer: {str(v.Odometer)} {v.OdometerType}\n"
+    desc += f"→ Price: {str(v.Price)}\n"
+    desc += f"→ Sticker Price: {str(v.MonroneyPrice)}\n"
+    desc += f"→ Exterior: {separator.join(v.PAINT)}\n"
+    desc += f"→ Interior: {separator.join(v.INTERIOR)}\n"
+    desc += f"→ Additional Options: {separator.join(v.ADL_OPTS)}\n"
+    desc += f"→ Location: {v.City}, {v.StateProvince}"
+
+    card = {
+        "title": f"{v.Year} {v.Model.upper()} {v.TrimName} - {separator.join(v.PAINT)}",
+        "description": desc,
+        "url": f"https://www.tesla.com/{v.Model}/order/{v.VIN}?postal={zip_code}",
+        "color": None
+    }
+
+    return card
+
 
 def send_message(api_key, search_query, search_results):
+    total_matches = int(search_results.total_matches_found)
+
     msg = {
-        "content": f"{search_results.total_matches_found} {search_query.query.condition} {search_query.query.model} found within {search_query.query.range} miles of {search_query.query.zip} in {search_query.query.region}",
+        "content": f"{str(total_matches)} {search_query.query.condition} {search_query.query.model.upper()} found within {search_query.query.range} miles of {search_query.query.zip} in {search_query.query.region}",
         "embeds": [
             {
-                "title": f"View all {search_query.query.model} inventory",
+                "title": f"View all {search_query.query.model.upper()} inventory",
                 "description": "This link will take you to Tesla's inventory search page.",
                 "url": f"https://www.tesla.com/inventory/{search_query.query.condition}/{search_query.query.model}?arrangeby=phl&zip={search_query.query.zip}&range={str(search_query.query.range)}",
                 "color": None
@@ -15,25 +39,59 @@ def send_message(api_key, search_query, search_results):
         ]
     }
 
-    for x in range(int(search_results.total_matches_found)):
+    for x in range(total_matches):
         if x == 9:
             break
-        v = search_results.results[x]
-        separator = ", "
-        desc = f"→ VIN: {v.VIN}\n"
-        desc += f"→ Odometer: {str(v.Odometer)} {v.OdometerType}\n"
-        desc += f"→ Price: {str(v.Price)}\n"
-        desc += f"→ Sticker Price: {str(v.MonroneyPrice)}\n"
-        desc += f"→ Exterior: {separator.join(v.PAINT)}\n"
-        desc += f"→ Interior: {separator.join(v.INTERIOR)}\n"
-        desc += f"→ Additional Options: {separator.join(v.ADL_OPTS)}\n"
-        desc += f"→ Location: {v.City}"
-        msg["embeds"].append({
-            "title": f"{v.Year} {v.TrimName} - {separator.join(v.PAINT)}",
-            "description": desc,
-            "url": f"https://www.tesla.com/{v.Model}/order/{v.VIN}?postal={search_query.query.zip}",
-            "color": None
-        })
+        msg["embeds"].append(build_vehicle_card(
+            search_results.results[x], search_query.query.zip))
+
+    conn = http.client.HTTPSConnection("discord.com")
+    payload = json.dumps(msg)
+    headers = {
+        'Content-Type': 'application/json'
+    }
+    conn.request("POST", api_key, payload, headers)
+    res = conn.getresponse()
+
+
+def send_message_split_results(api_key, search_query, search_results):
+    total_exact = len(search_results.results.exact)
+    total_approximate = len(search_results.results.approximate)
+    total_approximateOutside = len(search_results.results.approximateOutside)
+    total_split_matches = total_exact + total_approximate + total_approximateOutside
+
+    msg = {
+        "content": f"{str(total_split_matches)} {search_query.query.condition} {search_query.query.model.upper()} found within {search_query.query.range} miles of {search_query.query.zip} in {search_query.query.region}",
+        "embeds": [
+            {
+                "title": f"View all {search_query.query.model.upper()} inventory",
+                "description": "This link will take you to Tesla's inventory search page.",
+                "url": f"https://www.tesla.com/inventory/{search_query.query.condition}/{search_query.query.model}?arrangeby=phl&zip={search_query.query.zip}&range={str(search_query.query.range)}",
+                "color": None
+            }
+        ]
+    }
+
+    for x in range(total_exact):
+        if len(msg["embeds"]) == 10:
+            break
+
+        msg["embeds"].append(build_vehicle_card(
+            search_results.results.exact[x], search_query.query.zip))
+
+    for x in range(total_approximate):
+        if len(msg["embeds"]) == 10:
+            break
+
+        msg["embeds"].append(build_vehicle_card(
+            search_results.results.approximate[x], search_query.query.zip))
+
+    for x in range(total_approximateOutside):
+        if len(msg["embeds"]) == 10:
+            break
+
+        msg["embeds"].append(build_vehicle_card(
+            search_results.results.approximateOutside[x], search_query.query.zip))
 
     conn = http.client.HTTPSConnection("discord.com")
     payload = json.dumps(msg)
